2d6bac211dc18611cea872f5e8bb05aae01ef6a0
jwma <mjw@majiawei.com>
2017-04-02 21:51:03 +0800

    1. 添加前端页面并完成基础样式 2. 前端对接仓库列表接口，展现列表信息

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,31 +4,37 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Template()
      */
     public function indexAction(Request $request)
     {
-        return new Response('halo');
     }
 
     /**
      * @Route("/api/repo")
      * @Method("GET")
      */
-    public function listRepoAction()
+    public function listRepoAction(Request $request)
     {
+        if (!$this->isCsrfTokenValid('my_xsrftoken', $request->headers->get('xsrftoken'))) {
+            return new JsonResponse(null, 403);
+        }
+
         $em = $this->getDoctrine()->getManager();
 
         $list = $em->getRepository('AppBundle:Repo')->all();
 
-        return new JsonResponse($list);
+        return new JsonResponse([
+            'list' => $list
+        ]);
     }
 }
